796e78b4f7b3463b6da4f8feb557a6343a223c66
lucas <lucasmacielaguiar@gmail.com>
2019-07-11 17:18:55 -0300

    pass channel functions to sala.c

--- chat.c
+++ chat.c
@@ -9,7 +9,7 @@
 #include <fcntl.h>
 #include <dirent.h>
 #include <sys/types.h>
-#include <regex.h>
+#include "sala.c" // funcoes para controle do channel(create, receber e enviar mensagens)
 
 // constantes uteis
 #define size_message 523
@@ -29,15 +29,6 @@ struct
     char receiver[11]; //usuario destino
     char body[501];    //corpo da mensagem
 } typedef msgtp;
-
-// struct para guardar informacoes de canais
-struct
-{
-    char membros[200][11];
-    char path_channel[26];
-    int size_list_membros;
-    char full_msg[523]; // atributo auxiliar para passar mensagem entre as trheads do canal
-} typedef channeltp;
 
 void close_program(char *bye_msg)
 {
@@ -435,149 +426,6 @@ void *thenviar(void *dest_and_msg)
         mq_close(enviar);
     }
     pthread_exit(NULL);
-}
-
-void *thenviarChannel(void *channel_md)
-{
-    /*
-        thread que envia a mensagem para os membros
-    */
-
-    char membros[200][11]; //array com os membros do canal
-    int size_list_membros = 1;
-    // strcpy(membros[0], "lucas");
-    strcpy(membros[0], "lays");
-
-    channeltp *channel = (channeltp *)channel_md;
-
-    int response_send, try_send = 0;
-
-    mqd_t enviar;
-
-    char string_formated[600];
-    int i = 0;
-
-    char receiver_users[500][20];
-    int user_count = 0;
-    char envfila[17] = "";
-
-    for (i = 0; i < size_list_membros; i++)
-    {
-        //percorre os membros do canal
-
-        strcpy(envfila, "/chat-");
-        strcat(envfila, membros[i]);
-
-        if ((enviar = mq_open(envfila, O_WRONLY)) < 0)
-        {
-            perror("Erro ao encontrar usuario membro %s"); //erro de usuario inexistente
-            continue;
-        }
-
-        do
-        {
-            printf("MENSAGEM: %s\n", channel->full_msg);
-            response_send = mq_send(enviar, (void *)channel->full_msg, 523, 0);
-            try_send++;
-        } while (response_send < 0 && try_send < 3); //tenta enviar a mensagem 3 vezes
-
-        if (response_send < 0)
-        {
-            //erro retornado se não foi possível enviar mensagem
-            perror("Erro ao repassar mensagem para membro %s");
-            continue;
-        }
-        else
-        {
-            // usuarios que receberam a mensagem
-            strcpy(receiver_users[user_count], membros[i]);
-            user_count++;
-        }
-
-        mq_close(enviar);
-    }
-
-    printf("Mensagem enviada para o Channel: ");
-    for (int i = 0; i < user_count; i++)
-    {
-        printf("%s, ", receiver_users[i]);
-    }
-    printf("\n");
-
-    pthread_exit(NULL);
-}
-
-void *threceiverChannel(void *channel_md)
-{
-    //thread que consome a fila de mensagens do canal e prepara cada mensagem para envio
-    printf("Canal ativo\n");
-    msgtp msg;
-    char full_msg[523];
-    mqd_t receber;
-    pthread_t ids[2];
-
-    channeltp *channel = (channeltp *)channel_md;
-
-    //abre a fila para recebimento
-    if ((receber = mq_open((char *)channel->path_channel, O_RDWR)) < 0)
-    {
-        perror("mq_open error\n");
-        exit(1);
-    }
-
-    char string_formated[600];
-    while (1)
-    { //fica em loop esperando novas mensagens
-
-        if ((mq_receive(receber, (char *)full_msg, sizeof(full_msg), NULL)) < 0)
-        {
-            perror("mq_receive erro\n");
-            exit(1);
-        }
-        //Cria a mensagem no formato a ser enviada pros usuarios membros do canal
-        printf("MENSAGEM: %s\n", full_msg);
-        strcpy(channel->full_msg, (char *)full_msg);
-        pthread_create(&ids[1], NULL, thenviarChannel, (void *)channel);
-    }
-}
-
-void create_channel()
-{
-    channeltp channel;
-    strcpy(channel.path_channel, "/canal-"); //nome da fila do canal
-    char membros[200][11];                   //array com os membros do canal
-
-    char nomecanal[21];
-    printf("Digite o nome do canal: ");
-    scanf("%s", nomecanal);
-    // execl("sala", "sala", nomecanal, NULL);
-
-    pthread_t ids[2];
-
-    strcat(channel.path_channel, nomecanal); //junta o "/chat-" com o nome do usuario pra formar o nome da fila
-
-    mqd_t receber;
-    struct mq_attr attr;
-
-    attr.mq_maxmsg = 10;
-    attr.mq_msgsize = sizeof(msgtp);
-    attr.mq_flags = 0;
-
-    mode_t pmask = umask(0000);
-
-    //Cria e abre a fila para receber as mensagens, com os paramteros acima
-    if ((receber = mq_open(channel.path_channel, O_RDWR | O_CREAT | O_EXCL, 0622, &attr)) < 0)
-    {
-        perror("Erro ao criar Canal\n");
-        exit(1);
-    }
-
-    printf("Sala criada\n");
-
-    umask(pmask);
-
-    //inicia a thread que espera por mensagens
-    pthread_create(&ids[0], NULL, threceiverChannel, (void *)&channel);
 }
 
 int main()
--- sala.c
+++ sala.c
@@ -15,112 +15,100 @@ char nomesala[20];            //nome do canal
 char nomefila[26] = "canal-"; //nome da fila do canal
 char membros[200][11];        //array com os membros do canal
 
+// struct
+// {
+//     char sender[11];   //usuario origem
+//     char receiver[11]; //usuario destino
+//     char body[501];    //corpo da mensagem
+// } typedef msgtp;
+
+// struct para guardar informacoes de canais
 struct
 {
-    char sender[11];   //usuario origem
-    char receiver[11]; //usuario destino
-    char body[501];    //corpo da mensagem
-} typedef msgtp;
+    char membros[200][11];
+    char path_channel[26];
+    int size_list_membros;
+    char full_msg[523]; // atributo auxiliar para passar mensagem entre as trheads do canal
+} typedef channeltp;
 
-void split_format_message_full(char *full_msg, char *sender, char *dest,
-                               char *body, char *index_msg)
+void *thenviarChannel(void *channel_md)
 {
     /*
-        Funcao para cortar as mensagens por partes, atraves dos ":"
+        thread que envia a mensagem para os membros
     */
-    // mensagem no formato usuario_origem:usuario_destino:corpo_mensagem\n
-    // usuario de destino
-    char *token;
-    token = strtok(full_msg, ":");
-    if (token != NULL && sender != NULL)
-        strcpy(sender, token);
-    else
-        sender = "";
-    token = strtok(NULL, ":");
-    if (token != NULL && dest != NULL)
-        strcpy(dest, token);
-    else
-        dest = "";
-    token = strtok(NULL, ":");
-    if (token != NULL && body != NULL)
-        strcpy(body, token);
-    else
-        body = "";
-    token = strtok(NULL, ":");
-    if (token != NULL && index_msg != NULL)
-    {
-        strcpy(index_msg, token);
-    }
-    else
-        index_msg = "";
-}
 
-void *thenviar(void *dest_and_msg)
-{
-    /*
-        thread que envia a mensagem
-    */
+    // char membros[200][11]; //array com os membros do canal
+
+    channeltp *channel = (channeltp *)channel_md;
 
     int response_send, try_send = 0;
-    msgtp msg;
-
-    // Formato de Mensagem username:dest:<msg_content>:index:<msg_type>
-    char full_msg[523] = "";
-    sprintf(full_msg, "%s:%s:%d", nomefila, (char *)dest_and_msg, msg_index);
-
-    split_format_message((char *)dest_and_msg, msg.receiver, msg.body);
-    strcpy(msg.sender, nomefila);
 
     mqd_t enviar;
 
     char string_formated[600];
     int i = 0;
 
-    char envfila2[17] = "/";
+    char receiver_users[500][20];
+    int user_count = 0;
+    char envfila[17] = "";
 
-    for (i = 0; i < 200; i++)
+    for (i = 0; i < channel->size_list_membros; i++)
     {
         //percorre os membros do canal
 
-        strcpy(envfila2, "/");
-        strcat(envfila2, membros[i]);
+        strcpy(envfila, "/chat-");
+        strcat(envfila, channel->membros[i]);
 
-        if ((enviar = mq_open(envfila2, O_WRONLY)) < 0)
+        if ((enviar = mq_open(envfila, O_WRONLY)) < 0)
         {
+            perror("Erro ao encontrar usuario membro %s"); //erro de usuario inexistente
             continue;
         }
 
         do
         {
-            response_send = mq_send(enviar, (void *)full_msg, sizeof(full_msg), 0);
+            response_send = mq_send(enviar, (void *)channel->full_msg, 523, 0);
             try_send++;
         } while (response_send < 0 && try_send < 3); //tenta enviar a mensagem 3 vezes
 
         if (response_send < 0)
         {
             //erro retornado se não foi possível enviar mensagem
+            perror("Erro ao repassar mensagem para membro %s");
             continue;
+        }
+        else
+        {
+            // usuarios que receberam a mensagem
+            strcpy(receiver_users[user_count], channel->membros[i]);
+            user_count++;
         }
 
         mq_close(enviar);
     }
 
+    printf("Mensagem enviada para o Channel: ");
+    for (int i = 0; i < user_count; i++)
+    {
+        printf("%s, ", receiver_users[i]);
+    }
+    printf("\n");
+
     pthread_exit(NULL);
 }
 
-void *threceber(void *s)
+void *threceiverChannel(void *channel_md)
 {
-
     //thread que consome a fila de mensagens do canal e prepara cada mensagem para envio
-
-    msgtp msg;
+    printf("Canal ativo\n");
     char full_msg[523];
     mqd_t receber;
     pthread_t ids[2];
-    char msg_cmd[501] = "#"; //significa que é um canal
+
+    channeltp *channel = (channeltp *)channel_md;
 
     //abre a fila para recebimento
-    if ((receber = mq_open(nomefila, O_RDWR)) < 0)
+    if ((receber = mq_open((char *)channel->path_channel, O_RDWR)) < 0)
     {
         perror("mq_open error\n");
         exit(1);
@@ -135,65 +123,50 @@ void *threceber(void *s)
             perror("mq_receive erro\n");
             exit(1);
         }
-
-        char index_msg[10] = "";
-        // char msg_type[10];
-        split_format_message_full(full_msg, msg.sender, msg.receiver, msg.body, index_msg);
-
-        // Verifica se é uma mensagem normal:msg ou de verificar assinatura
-        if (index_msg[strlen(index_msg) - 1] == '?')
-        {
-            // É uma mensagem de verificacao '?', entao apenas
-            // verifique e envie a resposta 'y' ou 'n'
-            confirm_signature(msg.sender, msg.body, index_msg);
-        }
-        else if (index_msg[strlen(index_msg) - 1] == 'y' || index_msg[strlen(index_msg) - 1] == 'n')
-        {
-            //Cria a mensagem no formato a ser enviada pros usuarios membros do canal
-            strcat(msg_cmd, nomesala);
-            strcat(msg_cmd, ":");
-            strcat(msg_cmd, msg.sender);
-            strcat(msg_cmd, ":");
-            strcat(msg_cmd, msg.body);
-
-            pthread_create(&ids[1], NULL, thenviar, (void *)msg_cmd);
-        }
-        else
-        {
-            // É uma mensagem comum, entao cheque a autenticidade da mensagem
-            // enviando uma mensagem de check "?"
-            check_signature(msg.sender, msg.body, index_msg);
-        }
+        //Cria a mensagem no formato a ser enviada pros usuarios membros do canal
+        strcpy(channel->full_msg, (char *)full_msg);
+        pthread_create(&ids[1], NULL, thenviarChannel, (void *)channel);
     }
-
-    pthread_exit(NULL);
 }
 
-int main(int argc, char **argv)
+void create_channel()
 {
+    channeltp channel;
+    channel.size_list_membros = 0; // incialmente a lista de mebros eh zero
 
-    strcpy(nomesala, argv[1]);
+    strcpy(channel.path_channel, "/canal-"); //nome da fila do canal
+    // char membros[200][11];                   //array com os membros do canal
+    strcpy(channel.membros[channel.size_list_membros++], "lays");
+
+    char nomecanal[21];
+    printf("Digite o nome do canal: ");
+    scanf("%s", nomecanal);
+    // execl("sala", "sala", nomecanal, NULL);
+
     pthread_t ids[2];
 
-    strcat(nomefila, nomesala); //junta o "/chat-" com o nome do usuario pra formar o nome da fila
+    strcat(channel.path_channel, nomecanal); //junta o "/chat-" com o nome do usuario pra formar o nome da fila
 
     mqd_t receber;
     struct mq_attr attr;
 
     attr.mq_maxmsg = 10;
-    attr.mq_msgsize = sizeof(msgtp);
+    attr.mq_msgsize = 523;
     attr.mq_flags = 0;
 
     mode_t pmask = umask(0000);
 
     //Cria e abre a fila para receber as mensagens, com os paramteros acima
-    if ((receber = mq_open(nomefila, O_RDWR | O_CREAT | O_EXCL, 0622, &attr)) < 0)
+    if ((receber = mq_open(channel.path_channel, O_RDWR | O_CREAT | O_EXCL, 0622, &attr)) < 0)
     {
+        perror("Erro ao criar Canal\n");
         exit(1);
     }
+
+    printf("Sala criada\n");
 
     umask(pmask);
 
     //inicia a thread que espera por mensagens
-    pthread_create(&ids[0], NULL, threceber, NULL);
+    pthread_create(&ids[0], NULL, threceiverChannel, (void *)&channel);
 }
